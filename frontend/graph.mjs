5d647322d321ebb44389d9d9e79b50b23fbde848
aparvate <amogh.parvate@gmail.com>
2023-09-17 16:34:29 -0500

    Graph update

--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,8 +1,22 @@
 const graphDiv = document.getElementById("graph");
 
+var layout = {
+    title:'COVID-19 Hospitalizations vs Deaths',
+    xaxis: {
+        title: 'COVID-19 Hospitalizations',
+        showgrid: false
+      },
+      yaxis: {
+        title: 'COVID-19 Deaths',
+        showline: false
+      }
+};
+
+const config = {scrollZoom: true, responsive: true};
+
 fetch(
     "https://aparvate-dssd-oa-2023-24.onrender.com/" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
 ).then(async res => {
-    Plotly.newPlot(graphDiv, [await res.json()]); 
+    Plotly.newPlot(graphDiv, [await res.json()], layout, config); 
 })
 
